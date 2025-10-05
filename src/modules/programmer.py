852c9128631b910ef3bd65c541b1e3636b7f1725
ahmaadd10x <ahmadhds909@gmail.com>
2025-10-06 01:21:16 +0300

    did conversion Octal to Decimal

--- src/modules/programmer.py
+++ src/modules/programmer.py
@@ -6,7 +6,6 @@ Handles base conversions and bitwise operations
 
 
 
-
 def programmer_menu():
 
     """Main menu for programmer calculator mode"""
@@ -21,28 +20,24 @@ def programmer_menu():
         try:
             print("="*30)
             choice = int(input("Select an option (1-3): "))
-
             if choice == 1:
                 value1=base_conversions_menu()
                 print(value1)
                 break
-
             elif choice == 2:
                 value2=bitwise_operations_menu()
                 print(value2)
                 break
-
             elif choice == 3:
                 return
-            
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 3.")
             continue
         except KeyboardInterrupt:
             print("Exiting to Main Menu.")
             return
-        break
 
+        break
 
 
 
@@ -52,7 +47,7 @@ def base_conversions_menu():
     print("="*30)
     print("Base Conversion")
     print("="*30)
-    print("1. Binary to Decimal/Octal/Hexadecimal\n")
+    print("1. Binary to Decimal/Octal\n")
     print("2. Decimal to Binary/Hexadecimal/Octal\n")
     print("3. Hexadecimal to Binary/Decimal/Octal\n")
     print("4. Octal to Binary/Decimal/Hexadecimal\n")
@@ -62,30 +57,20 @@ def base_conversions_menu():
         try:
             print("="*30)
             choice=int(input("Select an option (1-5): "))
-
             if choice == 1:
-                value = binary_to_others_menu()
-                print(value)
-                break
-
+                binary_to_others_menu()
             elif choice == 2:
                 value = decimal_to_others_menu()
                 print(value)
                 break
-
             elif choice == 3:
-                value = hexadecimal_to_others_menu()
-                print(value)
-                break
-
+                hexadecimal_to_others_menu()
             elif choice == 4:
-                value = octal_to_others_menu()
+                value=octal_to_others_menu()
                 print(value)
                 break
-
             elif choice == 5:
                 return
-            
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 5.")
             continue
@@ -97,23 +82,19 @@ def base_conversions_menu():
             
 
 
-def decimal_to_others_menu():
 
+def decimal_to_others_menu():
     """ Converts Decimal to Binary, Hexadecimal, and Octal """
     print("="*30)
     print("1. Decimal to Binary")
     print("2. Decimal to Hexadecimal")
     print("3. Decimal to Octal")
     print("4. Back to Base Conversion Menu")
-
     while True:
         try:
             print("="*30)
             choice=int(input("Select an option (1-4): "))
-
-
             if choice == 1:
-                """Convert decimal to binary"""
                 decimalNumberUser=int(input("Enter a Decimal number: "))  
                 decimalNumber=abs(decimalNumberUser)
 
@@ -134,10 +115,9 @@ def decimal_to_others_menu():
                     remainder = decimalNumber % 2 # Get Remainder 0 or 1
                     result += str(remainder) # Append Remainder to Result String
                     decimalNumber = decimalNumber // 2 # Refresh Decimal Number by Floor Division
-                result = result[::-1] # Reverse the Result to get the correct order
+                result = result[::-1] # Reverse the Result List to get the correct order
                 return result
             
-
             elif choice == 2:
                 """Convert decimal to hexadecimal"""
                 decimalNumberUser=int(input("Enter a Decimal number: "))
@@ -164,40 +144,69 @@ def decimal_to_others_menu():
             
 
             elif choice == 3:
-                """Convert decimal to octal"""
-                decimalNumberUser=int(input("Enter a Decimal number: "))
-                decimalNumber=abs(decimalNumberUser)
-
-                if decimalNumber == 0:
-                    return "0"
-                
-                result=""
-
-                while decimalNumber > 0:
-                    """
-                    Example => Decimal 8
-                    8 / 8 = 1 remainder 0
-                    1 / 8 = 0 remainder 1
-                    Result = 10
-                    """
-                    remainder = decimalNumber % 8 # Get Remainder between 0-7
-                    result += str(remainder) # Append Remainder to Result String
-                    decimalNumber = decimalNumber // 8 # Refresh Decimal Number by Floor Division
-                result=result[::-1] # Reverse the Result to get the correct order
-                return result
-            
-
+                return 
             elif choice == 4:
                 return
-            
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 4.")
             continue
         except KeyboardInterrupt:
             print("Exiting to Base Conversion Menu.")
             return
-        break
-        
+def octal_to_Decimal():
+    octal_number = input('Enter your octal number ')
+    octal_number = str(octal_number)
+    octal_number = octal_number[::-1]
+    decimal_number = 0
+    try:
+        for i in range(len(octal_number)):
+            # since octal is base 8 we have 8**i for evrey index
+            # multiply by the number in the same index  
+            # for example number: 70 we reverse the number to 07
+            # then (8**0 *0)(loop1) +8**1 * 7(loop2)= 56  
+            if int(octal_number[i]) <= 7: # check if it's a valid digit
+                decimal_number += 8**i * int(octal_number[i])
+            else:
+                print('Octal number can’t have 8 or 9, please enter a number that has only 0–7.')
+                return
+        return decimal_number
+    except ValueError:
+        print('Invalid input. Octal number has digits range of 0..7.')
+        return
+    except KeyboardInterrupt:
+        print("Exiting to Base Conversion Menu.")
+        return
 
+def octal_to_others_menu():
+    '''Converts Octal to Decimal,Binary and Hexadecimal'''
+    print("="*30)
+    print("1. Octal to Decimal")
+    print("2. Octal to Hexadecimal")
+    print("3. Octal to Binary")
+    print("4. Back to Base Conversion Menu")
+    while True :
+        try:
+            print("="*30)
+            choise = int(input(" Select an option (1-4): "))
+            if choise==1:
+                return octal_to_Decimal()
+                
+
+            elif choise==2:
+                return
+
+            elif choise==3:
+                return
+
+            elif choise==4:
+                print("Returning to Base Conversion Menu...")
+                break
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 4.")
+            continue
+        except KeyboardInterrupt:
+             print("Exiting to Base Conversion Menu.")
+             return
+    return       
 programmer_menu()
 
